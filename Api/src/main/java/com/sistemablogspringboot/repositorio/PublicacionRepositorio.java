e02f0993491235ea374bae5b91bcf3fbe4e62fa7
Fabricio Barreto <80056941+fabrib26@users.noreply.github.com>
2022-06-29 18:16:20 -0300

    Delete Api directory

--- Api/src/main/java/com/sistemablogspringboot/repositorio/PublicacionRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.sistemablogspringboot.repositorio;
-
-import com.sistemablogspringboot.entidades.Publicacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublicacionRepositorio extends JpaRepository<Publicacion,Long> {
-}
--- Api/src/main/java/com/sistemablogspringboot/servicio/PublicacionServicio.java
+++
@@ -1,9 +0,0 @@
-package com.sistemablogspringboot.servicio;
-
-import com.sistemablogspringboot.dto.PublicacionDTO;
-
-public interface PublicacionServicio {
-
-    public PublicacionDTO crearPublicacion(PublicacionDTO publicacionDTO);
-
-}
